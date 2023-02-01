3ef0d885123d69db9fdc6d27250b690d526b665a
Amber Shipley <ashipley2@me.com>
2023-02-01 15:11:26 -0800

    Create more game tests, delete comments and createCards function, and create instance of Round

--- src/Game.js
+++ src/Game.js
@@ -7,30 +7,22 @@ const Card = require('./Card')
 
 class Game {
   constructor() {
+    this.currentRound;
   }
 
   start() {
-    // Creates Cards
     const cards = prototypeQuestions.map(question => {
       return new Card(question.id, question.question, question.answers, question.correctAnswer)
     });
     const deck = new Deck(cards);
-    console.log(deck)
-    // Puts Cards in a Deck
-    // Creates a new Round using the Deck
-    this.currentRound = new Round();
-    // invokes printMessage to display the message in the CLI
-    // invokes printQuestion to kick off our helper functions that allow interaction via the CLI
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    // this.printQuestion();
   }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
-  }
-
-  createCards() {
-   
-    let deck = new Deck(cards);
   }
 
   printQuestion(round) {
--- test/Game-test.js
+++ test/Game-test.js
@@ -3,13 +3,27 @@ const assert = chai.assert;
 
 const Game = require('../src/Game');
 const Round = require('../src/Round')
+const Card = require('../src/Card')
 
 describe('Game', () => {
-    it('should keep track of the current round', () => {
-        const game = new Game();
+    let game;
+    beforeEach(() => {
+        game = new Game();
+    });
+
+    it('should be able to start a new game', () => {
         assert.equal(game.currentRound, undefined);
         game.start();
+        assert.instanceOf(game.currentRound, Round)
+    });
 
+    it('should create and put cards in deck when starting game', () => {
+        game.start();
+        assert.instanceOf(game.currentRound.deck[0], Card)
+    });
+
+    it('should keep track of the current round', () => {
+        game.start();
         assert.instanceOf(game.currentRound, Round);
     });
 });
